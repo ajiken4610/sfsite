6ae4efe7b513057dae0b11bfa6b9a62f7b4d9fd3
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-17 08:15:12 +0900

    PWA対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,26 @@ import { defineNuxtConfig } from "nuxt";
 export default defineNuxtConfig({
   ssr: false,
   builder: "webpack",
+  modules: [
+    [
+      "@kevinmarrec/nuxt-pwa",
+      {
+        icon: { source: "public/icon.png" },
+        meta: {
+          name: "Salesio Festa",
+          theme_color: "#000088",
+          lang: "ja",
+          mobileAppIOS: true,
+        },
+        manifest: {
+          name: "Salesio Festa 2022",
+          short_name: "SF2022",
+          description: "サレ祭2022年度",
+        },
+        workbox: {
+          enabled: true,
+        },
+      },
+    ],
+  ],
 });
