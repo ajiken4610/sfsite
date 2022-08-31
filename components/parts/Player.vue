827684a25deaced2a0374abf840bb96a2fd1f7ba
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-31 18:49:19 +0900

    投票画面トリマ作った

---
+++ components/parts/Player.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+.d-lg-flex(:class="[{ 'flex-row-reverse': reversed }]")
+  .flex-fill.position-relative.d-lg-block.d-none
+    .position-absolute.top-50.start-50.translate-middle.w-100
+      .display-1.text-center {{ name }}
+      .h1.lead.text-muted.text-center {{ description }}
+  .flex-fill.d-lg-none.d-block
+    .display-1.text-center {{ name }}
+    .h1.lead.text-muted.text-center {{ description }}
+  img.mr-image.d-lg-block.d-none.w-50
+  img.mr-image.d-lg-none.d-block.w-100
+</template>
+<script setup lang="ts">
+defineProps<{ name: string; description: string; reversed: boolean }>();
+</script>
+<style scoped lang="scss">
+.mr-image {
+  display: block;
+  height: 33vh;
+}
+</style>
---
+++ pages/mr.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+div
+  .display-1 Mr.Salesio 投票!
+  .lead.mb-4 Mrサレジオ投票にご参加ください!
+  template(v-for="(player,index) of players")
+    hr
+    Player.my-4(v-bind="player", :reversed="index % 2 === 0")
+</template>
+
+<script setup lang="ts">
+import Player from "~~/components/parts/Player.vue";
+const players = [
+  { name: "サレジオ太郎1", description: "1ろーれむいぷさむ" },
+  { name: "サレジオ太郎2", description: "2ろーれむいぷさむ" },
+  { name: "サレジオ太郎3", description: "3ろーれむいぷさむ" },
+  { name: "サレジオ太郎4", description: "4ろーれむいぷさむ" },
+  { name: "サレジオ太郎5", description: "5ろーれむいぷさむ" },
+];
+
+for (var i = 0; i < players.length; i++) {
+  const rad = Math.floor(Math.random() * players.length);
+  [players[i], players[rad]] = [players[rad], players[i]];
+}
+</script>
