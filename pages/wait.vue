99f08d1ff5389cf42597e7da007dd34393978f86
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-09-03 13:30:11 +0900

    待ち時間が時間になるとーになるのを阻止

--- pages/wait.vue
+++ pages/wait.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 div 
   h1.text-center サレ祭開始までお待ちください...
-  .text-center.lead サレ祭開始まで{{ Math.floor(toFes / 3600) }}時間{{ Math.floor((toFes / 60) % 60) }}分{{ toFes % 60 }}秒
   NuxtLink(v-if="toFes < 0", to="/home")
     button.btn.btn-outline-primary.mx-auto.d-block 進む
+  .text-center.lead(v-else) サレ祭開始まで{{ Math.floor(toFes / 3600) }}時間{{ Math.floor((toFes / 60) % 60) }}分{{ toFes % 60 }}秒
 </template>
 
 <script setup lang="ts">
