24235d341fa0df9a2854d10c3c66716b4dec0f78
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-09-03 10:24:15 +0900

    待機画面にカウントと誘導ボタンを追加

--- pages/wait.vue
+++ pages/wait.vue
@@ -1,4 +1,21 @@
 <template lang="pug">
 div 
-  h1 サレ祭開始までお待ちください...
+  h1.text-center サレ祭開始までお待ちください...
+  .text-center.lead サレ祭開始まで{{ Math.floor(toFes / 3600) }}時間{{ Math.floor((toFes / 60) % 60) }}分{{ toFes % 60 }}秒
+  NuxtLink(v-if="toFes < 0", to="/home")
+    button.btn.btn-outline-primary.mx-auto.d-block 進む
 </template>
+
+<script setup lang="ts">
+const toFes = ref(0);
+let running = true;
+const render = () => {
+  running && requestAnimationFrame(render);
+  toFes.value = Math.floor((1663369200000 - Date.now()) / 1000);
+};
+render();
+
+onUnmounted(() => {
+  running = false;
+});
+</script>
